f5cf78dbee61f2ce21f0942e14b599b087f8cd95
daniels <daniels@xmind.net>
2024-08-07 23:45:28 +1000

    Impl: basic codes

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Header from './components/Header.vue';
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-slate-200 dark:bg-black dark:text-white">
+    <Header />
+    <main class="container mx-auto px-20">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+          <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+      </div>
+      <HelloWorld msg="Vite + Vue" />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="flex justify-between items-center p-4 bg-slate-100 dark:bg-black dark:text-white border-blue-500 dark:border-blue-400">
+    <div class="flex items-center gap-4 pl-8">
+      <img src="/vite.svg" class="logo" alt="logo"/>
+      <h1 class="text-4xl font-bold">Rates</h1>
+    </div>
+  </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.logo {
+  width: 2rem;
+  height: 2rem;
+}
+</style>
